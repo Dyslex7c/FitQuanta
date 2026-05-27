d298113bcad2f5aa7d44792fd16c45e3c12c891d
Dyslex7c <polynsominiac@gmail.com>
2026-05-27 17:40:22 +0530

    add badges button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -69,6 +69,7 @@ export default function Navbar() {
         { label: 'Trainers', href: '/trainer' },
         { label: 'Messages', href: '/chat' },
         { label: 'Purchases', href: '/purchases' },
+        { label: 'Badges', href: '/badges' },
         ...(user.onboardingComplete === false ? [{ label: 'Onboarding', href: '/onboarding', isWarning: true }] : [])
       ];
     }
